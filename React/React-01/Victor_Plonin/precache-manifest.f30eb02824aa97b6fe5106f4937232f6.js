5b5eb7950acafffacdc3c101f89b57ce4c644695
Aleksandr Serov <sashaserov2010@gmail.com>
2019-02-22 21:05:42 +0300

    React-01 | Victor Plonin

---
+++ React/React-01/Victor_Plonin/precache-manifest.f30eb02824aa97b6fe5106f4937232f6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e9af15d6ff319717d3a674cf089857e",
+    "url": "./static/media/pleasure_bg.6e9af15d.jpg"
+  },
+  {
+    "revision": "b69bd070042bf27627c518c04add96f2",
+    "url": "./static/media/paper.b69bd070.jpg"
+  },
+  {
+    "revision": "6bed7ac4bb8f91eddc1e348e5395147c",
+    "url": "./static/media/contact_bg.6bed7ac4.jpg"
+  },
+  {
+    "revision": "2fdcd636b72b8452c9d51e5635185ddb",
+    "url": "./static/media/Main_bg.2fdcd636.jpg"
+  },
+  {
+    "revision": "7413c67d7da6e7c04269d1a4079069b2",
+    "url": "./static/media/Coffee_bg.7413c67d.jpg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "4daf09d55aca32efc512",
+    "url": "./static/js/main.4daf09d5.chunk.js"
+  },
+  {
+    "revision": "df1862b4afa83cf3494f",
+    "url": "./static/js/1.df1862b4.chunk.js"
+  },
+  {
+    "revision": "4daf09d55aca32efc512",
+    "url": "./static/css/main.64b8cffe.chunk.css"
+  },
+  {
+    "revision": "1313bda26c9c614f7a4ca20242a012e2",
+    "url": "./index.html"
+  }
+];
